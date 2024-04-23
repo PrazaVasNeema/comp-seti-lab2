3bb698132840bc7aed7abdbc31f1554e5105159d
Praza <keelahuagain@gmail.com>
2024-04-23 16:33:42 +0300

    1

---
+++ Assets/Scripts/Lab1DataSO.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Lab1DataSO", menuName = "Lab1/Lab1DataSO")]
+public class Lab1DataSO : ScriptableObject
+{
+    public enum InvestigatedValue
+    {
+        Ux,
+        P_prosti
+    }
+    public enum DependencyValue
+    {
+        lambda,
+        D,
+        mean,
+        std_dev
+    }
+    [SerializeField] private Data m_data;
+    public Data data => m_data;
+    
+    [System.Serializable]
+    public class Data
+    {
+        [Header("Параметры распределения")] 
+        [Header("A(x): Erland5 + D")] 
+        public float lambda;
+        public float D;
+        [Header("B(x): |Gauss|")]
+        public float mean;
+        public float std_dev;
+        [Header("Построение графиков")]
+        [Header("Количество итераций на одном наборе параметров")]
+        public float iterAmount;
+        [Header("Изменять параметры")]
+        public float from;
+        public float to;
+        public float step;
+        [Header("Количество заданий для остановки")]
+        public float k;
+        [Header("Исследуемое значение")]
+        public InvestigatedValue investigatedValue;
+        [Header("График зависит от параметра:")]
+        public DependencyValue dependencyValue;
+
+
+    }
+}
